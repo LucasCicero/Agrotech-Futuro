049bb1d142d3260c75f1278e7182272e78e6afcf
lucas <lucas.cicero94@gmail.com>
2023-11-20 14:59:34 -0300

    Mobile e Carrossel

--- js/main.js
+++ js/main.js
@@ -1,17 +1,37 @@
-//Código que faz o carrosel funcionar
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false, //true adiciona botoes para navegar
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  
+  showSlides(slideIndex = n);
+}
+
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display="block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+
